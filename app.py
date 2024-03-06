b5513e8032b1914e6323721886ee2d6d26f2838d
mutethiakigea <mutethiadicksonkigea@gmail.com>
2024-03-06 15:26:00 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import numpy as np
+from flask import Flask, request, render_template
+import pickle
+
+#Create an app object using the Flask class. 
+app = Flask(__name__)
+
+#Load the trained model. (Pickle file)
+model = pickle.load(open('model.pkl', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    features = [np.array(int_features)]
+    prediction = model.predict(features)
+    
+    output = prediction[0]
+    
+    if output == 0:
+        prediction_text = 'Diabetic'
+    else:
+        prediction_text = 'NotDiabetic'
+    
+    return render_template('index.html', prediction_text=prediction_text)
+
+
+if __name__ == "__main__":
+    app.run()
